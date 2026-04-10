16cd40875dffde66a626ec790f06ef7b4568ae7d
Tuco2006 <mateussmcruz@icloud.com>
2026-04-10 10:48:53 -0300

    adicionando barbeiros 4

--- backend/src/routes/barberRoutes.js
+++ backend/src/routes/barberRoutes.js
@@ -5,5 +5,6 @@ import { getPublicBarbers } from '../controllers/barberController.js';
 const router = Router();
 
 router.get('/', getPublicBarbers);
+router.get('/active', getPublicBarbers);
 
 export default router;
